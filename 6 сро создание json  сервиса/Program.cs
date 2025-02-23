3c79cbaaf52f047e056cf2633dfd9414da02a3f8
Tagaeva Aidana <tagaeva.aidana69@gmail.com>
2025-02-23 20:53:54 +0500

    добавила bestfriend

---
+++ 6 сро создание json  сервиса/Program.cs
@@ -0,0 +1,16 @@
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+//app.MapGet("/hello", () => new { message = "Hello, World!" });
+//app.Run();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapGet("/hello", () => new { message = "Hello,dear friend! and bestfriend!" });
+app.Run();
